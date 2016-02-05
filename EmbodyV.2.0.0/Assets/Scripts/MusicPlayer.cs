349dbcee260c93d014bdede86afd7f91031287d2
Embody <thomasle2324@gmail.com>
2016-02-04 19:46:46 -0800

    Main Menu Music
    -Added Main Menu Music
    -Does not get destroyed on load, will carry on throughout gameplay

---
+++ EmbodyV.2.0.0/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.DontDestroyOnLoad(gameObject); //music from main menu keeps playing instead of being destroyed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
